92fc0b0b2beb04111b714f4ec700a952a762f0cf
Dakota Thorpe <dakotatubestore@gmail.com>
2023-11-09 00:34:41 -0400

    commit

--- include/config.h
+++ include/config.h
@@ -2,6 +2,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+// arch
 #ifdef __i386__
 #define TARGET "x86 (32-bit)"
 #elif defined(__x86_64__)
@@ -22,6 +23,6 @@
 // config for different functions
 #define USE_THREADING
 #define USE_VIDEO
-//#define VIDEO_SDL2
+#define VIDEO_SDL2
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -11,12 +11,15 @@
  */
 void printCoreInfo() {
     // info
-    printf("%s.\nGCC version: %d.%d.%d\nCompiled on %s at %s\n\n", VERSION, __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__, __DATE__, __TIME__);
+    printf("%s %s.\nGCC version: %d.%d.%d\nCompiled on %s at %s\n\n", VERSION, TARGET, __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__, __DATE__, __TIME__);
 
     // print capabilities
     printf("Core Capabilities:\n");
     #ifdef USE_THREADING
     printf("\tThreading.c - 2023 Dakota Thorpe.\n");
+    #endif
+    #ifdef USE_VIDEO
+    printf("\tVideo.c - 2023 Dakota Thorpe.\n");
     #endif
     printf("\n");
 }
